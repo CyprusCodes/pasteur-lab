6cfe6411c8f2681ffaf1717d3461fd54f5ba0c8e
Aishah <maishah2540@gmail.com>
2026-01-02 11:03:05 +0200

    debug

--- src/utils/assets.ts
+++ src/utils/assets.ts
@@ -1,5 +1,7 @@
 export const getImagePath = (path: string) => {
   const base = import.meta.env.VITE_S3_BUCKET_ASSET_URL.replace(/\/$/, "");
+  console.log(import.meta.env.VITE_S3_BUCKET_ASSET_URL);
+  console.log("Base URL:", base);
   const cleanPath = path.replace(/^\//, "");
   return `${base}/${cleanPath}`;
 };
